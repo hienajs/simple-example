ae6fb66479d466f3991444ffd5bcf43efe72ccfd
Raul Silva Bunicenha <raulbunicenha@gmail.com.br>
2018-08-23 17:22:05 -0300

    Rotas Protegidas

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,11 @@
 import home from './home'
 import usuario from './usuario'
 import auth from './auth'
+import tarefa from './tarefa'
 
 export default {
   home,
   usuario,
-  auth
+  auth,
+  tarefa
 }
